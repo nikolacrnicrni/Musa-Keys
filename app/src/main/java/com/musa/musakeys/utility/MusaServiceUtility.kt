584153dc7159397deb9f899b2a8c350377c01909
nikolacrnicrni <crnogorac.nikola93@gmail.com>
2024-08-10 14:50:28 +0200

    Musa update

--- app/src/main/java/com/musa/musakeys/utility/MusaServiceUtility.kt
+++ app/src/main/java/com/musa/musakeys/utility/MusaServiceUtility.kt
@@ -1,26 +1,12 @@
 package com.musa.musakeys.utility
 
-import android.Manifest
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.core.content.ContextCompat
 import com.musa.musakeys.constants.MusaConstants
 import com.musa.musakeys.services.OrientationListenerRegistrationService
 
 object MusaServiceUtility {
-
-    private fun hasLocationPermission(context: Context): Boolean {
-        return ContextCompat.checkSelfPermission(
-            context,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
-            context,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    }
 
     private fun isOrientationChangeListenerServiceRunning(context: Context): Boolean {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
@@ -36,7 +22,7 @@ object MusaServiceUtility {
     }
 
     private fun startMusaPhoneBackgroundService(context: Context) {
-        if (!isOrientationChangeListenerServiceRunning(context) && hasLocationPermission(context)) {
+        if (!isOrientationChangeListenerServiceRunning(context)) {
             val intent = Intent(context, OrientationListenerRegistrationService::class.java).apply {
                 action = MusaConstants.START_FOREGROUND_ACTION
             }
